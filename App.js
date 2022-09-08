297f758937e5d0235e176ae49d554a6301e24f24
DoBaKien <kiendoba1905@gmail.com>
2022-09-08 19:43:11 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Alert } from 'react-native';
 
 export default function App() {
+
+  const [userName, setUserName] = React.useState("");
+  const [passWord, setPassWord] = React.useState("");
+
+  function callFunctionJs() {
+
+    if(userName == "Abc123" && passWord == "123456"){
+      console.log('Dang nhap thanh cong');
+      Alert.alert(
+       `Thành công ${userName}`
+     )
+    }
+    else{
+      console.log('Dang nhap khong thanh cong');
+      Alert.alert(
+        `sai ${userName}`
+      )
+    }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Login</Text>
+      <TextInput placeholder='User name' style={styles.txtip} onChangeText={(e) => setUserName(e)}></TextInput>
+      <TextInput placeholder='Password'style={styles.txtip} secureTextEntry={true} onChangeText={(e) => setPassWord(e)}></TextInput>
+      
+      <Button title='Login' onPress={callFunctionJs}></Button>
     </View>
   );
 }
@@ -17,4 +41,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txtip: {
+    borderColor: '#000000',
+    borderWidth: 3,
+    height: 40,
+    margin: 12,
+    padding: 10, 
+    width: 300,
+  }
 });
